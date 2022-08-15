ec8d7ba6b1183791423be82ffc57a798d96f4b57
Vicente Flores <luisvix0@gmail.com>
2022-08-14 22:05:03 -0500

    feat: mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { loadMongoConfig } from './config/helpers/load-mongo-config.helper';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: loadMongoConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
